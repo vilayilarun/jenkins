d35a3fe8f74462893235eb317710a84035ab227c
vilayilarun <vilayilarun@gmail.com>
2021-09-27 17:04:58 +0530

    corrected script.grrovy

--- script.groovy
+++ script.groovy
@@ -9,7 +9,7 @@ def buildapp() {
 def pushapp() {
     withCredentials([usernamePassword(credentialsId: 'Nexus-repo', usernameVariable: 'USER', passwordVariable: 'PWD')]){
     sh "echo $PWD | docker login -u $USER --password-stdin 192.168.179.131:8083"
-    sh 'docker push 192.168.179.131:8083/myapp:1.0
+    sh 'docker push 192.168.179.131:8083/myapp:1.0'
     }
 }
 def deployapp() {
