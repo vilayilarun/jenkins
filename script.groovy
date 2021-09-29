022808539b5e393f9a0b06b9218b19264721b416
vilayilarun <32998015+vilayilarun@users.noreply.github.com>
2021-09-29 15:43:30 +0400

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -2,17 +2,17 @@ def testapp() {
     echo "Testing the code"
 }
 
-def buildapp() {
+//def buildapp() {
     //sh 'docker-compose -d --build'
-    sh 'docker build -t 192.168.179.131:8083/myapp:1.0 .'
-}
+   // sh 'docker build -t 192.168.179.131:8083/myapp:1.0 .'
+//}
 
-def pushapp() {
-    withCredentials([usernamePassword(credentialsId: 'Nexus-repo', usernameVariable: 'USER', passwordVariable: 'PWD')]){
-    sh "echo $PWD | docker login -u $USER --password-stdin 192.168.179.131:8083"
-    sh 'docker push 192.168.179.131:8083/myapp:1.0'
-    }
-}
+//def pushapp() {
+   // withCredentials([usernamePassword(credentialsId: 'Nexus-repo', usernameVariable: 'USER', passwordVariable: 'PWD')]){
+    //sh "echo $PWD | docker login -u $USER --password-stdin 192.168.179.131:8083"
+    //sh 'docker push 192.168.179.131:8083/myapp:1.0'
+    //}
+//}
 def deployapp() {
     sh 'echo deploying this'
 }
